7c697d6c96fbeb0fe769f3bfb8cb722bf5996dfd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-27 19:53:41 +0700

    Build starting `Navbar` structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.main`
 	overflow: hidden;
+	font-size: 1em;
 `;
 
 const App = () => {
---
+++ src/components/pages/Home/Navbar.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const Container = styled.nav`
+    position: absolute;
+    top: 0;
+    z-index: 10;
+    color: white;
+    fill: white;
+    width: 100%;
+    padding: 0.75em 0;
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+
+    > *:nth-of-type(1) {
+        flex-grow: 0.4;
+    }
+
+    #navbar-links {
+        align-items: center;
+        display: flex;
+        justify-content: space-evenly;
+        gap: 0.5em;
+        flex-grow: 1;
+    }
+`;
+
+const Navbar = ({ children: links, icon }) => {
+	return (
+		<Container id="navbar-home">
+			{icon}
+			<div id="navbar-links">{links}</div>
+		</Container>
+	);
+};
+
+export default Navbar;
