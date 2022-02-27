cd83cf503b9a039aabd68096d2628072e5081f02
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-27 15:34:54 +0700

    Implement fake trending show call

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,14 +1,40 @@
 import Poster from "./Poster";
+import axios from "axios";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const Container = styled.section``;
 
-const Home = ({ propsDestruct }) => {
+const Home = () => {
+	const [ info, setInfo ] = useState([]);
+
+	/* CMT The original goal of this function was to get trending shows, but I can't
+     seem to find an API for it that doesn't require authenciation so Imma fake it */
+	const getTrendingShows = async () => {
+		const shows = [ "the cuphead show", "euphoria", "what we do in the shadows", "WandaVision" ];
+		const trending = [];
+
+		for (let i = 0; i < shows.length; i++) {
+			try {
+				const res = await axios.get(
+					"https://api.tvmaze.com/singlesearch/shows",
+					{ params : { q : shows[i] } }
+				);
+
+				trending.push(res.data.image.original);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+
+		setInfo(trending);
+	};
+
+	useEffect(() => getTrendingShows(), []);
+
 	return (
 		<Container id="page-home">
-			{/* DBG */}
-			<Poster switchDuration={2000} fadeDuration={1000} info={[ "http://placekitten.com/200/300", "http://placekitten.com/400/600", "http://placekitten.com/200/300", "http://placekitten.com/400/600" ]}/>
-
+			<Poster switchDuration={5000} fadeDuration={1000} info={info}/>
 		</Container>
 	);
 };
